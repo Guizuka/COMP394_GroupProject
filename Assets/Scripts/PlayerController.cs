000ac461ef921dfc89b13e2294bb19dcbd5d608a
Guizuka <51218840+Guizuka@users.noreply.github.com>
2021-11-12 20:43:46 -0500

    Improved movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Ground Detection")]
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundRadius = 0.8f;
+    public bool isGrounded = false;
 
+    [Header("Movement")]
     public float speed = 5f;
     public float jumpHeight = 5f;
+    public bool facingRight = true;
 
+    Animator anim;
+
+    int JumpButton = Animator.StringToHash("JumpButton");
+    int AttkButton = Animator.StringToHash("AttkButton");
+
+
+    public GameObject player;
     private Rigidbody2D playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = this.GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRb = player.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerVelocity = playerRb.velocity;
-        playerVelocity.x = speed;
-        playerRb.velocity = playerVelocity;
+        Movement();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+
+    public void Movement()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
     }
 }
